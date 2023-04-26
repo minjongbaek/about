f3bc5867fe8cc7e00c07eff11589e44ee86ac9b4
minjongbaek <minjongbaek@gmail.com>
2023-04-26 17:16:00 +0900

    chore: svgr 패키지 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.tsx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
