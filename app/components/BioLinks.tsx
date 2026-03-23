3507008bc8f275ca9201d3f2ef20d750065554f4
minjongbaek <minjongbaek@gmail.com>
2026-03-23 20:10:39 +0900

    feat: BioLinks 컴포넌트에 외부 링크 rel 속성 추가

--- app/components/BioLinks.tsx
+++ app/components/BioLinks.tsx
@@ -16,7 +16,13 @@ export const BioLinks = ({ links }: BioLinksProps) => {
   return (
     <div className="flex flex-col gap-1">
       {links.map(({ label, href }) => (
-        <a key={label} href={href} target="_blank" className="flex gap-2">
+        <a
+          key={label}
+          href={href}
+          target="_blank"
+          className="flex gap-2"
+          rel="noopener noreferrer"
+        >
           <span className="font-semibold">{label}</span>
           <span className="text-gray-500">{getDisplayText(href)}</span>
         </a>
